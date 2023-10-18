4fe603d19e49b019e2bf6caf0c90bd617d0d2baa
Steven Large <slarge@sfu.ca>
2023-10-17 21:19:36 -0600

    ticked version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 setup(
     name='fokker-planck',
     packages=find_packages(),
-    version='1.0.5',
+    version='1.0.6',
     long_description=long_description,
     long_description_content_type='text/markdown',
     description='''
